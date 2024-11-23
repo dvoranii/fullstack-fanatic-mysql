eaedb2d2f965767f68b34ea50e892d96483e3e07
Ildi Dvorani <dvoranii@ildidev.com>
2024-11-23 13:23:05 -0500

    style updates to about

--- frontend/src/pages/About/AboutPage.styled.ts
+++ frontend/src/pages/About/AboutPage.styled.ts
@@ -1,40 +1,152 @@
 import styled from "styled-components";
+import { colors } from "../../GlobalStyles";
+import HexagonalBg from "../../assets/images/hexagonal-line-bg.jpg";
 
+interface ListSectionTitleWrapperProps {
+  offset?: string;
+  width?: string;
+}
+
+export const AboutIntroWrapperOuter = styled.div`
+  position: relative;
+
+  .elipse {
+    position: absolute;
+    transform: scaleX(-1);
+    width: 200px;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
+
+  .squares-triangle {
+    position: absolute;
+    top: 0;
+    left: 80px;
+    width: 250px;
+    transform: scaleX(-1);
+  }
+`;
 export const AboutIntroWrapper = styled.div`
   display: flex;
   align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 70vh;
+  gap: 2.4rem;
 `;
 
 export const AboutImageWrapper = styled.div`
   user-select: none;
   img {
-    width: 500px;
+    width: clamp(250px, 26vw, 500px);
   }
 `;
+export const LinkWrapper = styled.div`
+  margin-top: 1.2rem;
+  transition: all 250ms ease;
+  user-select: none;
+  width: fit-content;
 
+  &:hover {
+    transform: translateY(-2px);
+  }
+
+  a {
+    padding: 12px 24px;
+    margin-top: 1.2rem;
+    font-size: 1.2rem;
+    font-family: "Anybody";
+    border-radius: 30px;
+    background-color: ${colors.secondary};
+    color: ${colors.primary};
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    transition: all 250ms ease;
+
+    &:hover {
+      cursor: pointer;
+      box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
 export const MissionStatementWrapper = styled.div`
   font-size: 1.4rem;
 `;
 
 export const CoreTenetsWrapper = styled.div`
   width: 100%;
-  p {
-    text-align: center;
-    font-size: 1.4rem;
-    font-weight: bold;
-    color: #333;
-    padding-bottom: 2.4rem;
-    margin-top: 6.8rem;
-    border-bottom: 1px solid grey;
-    margin-bottom: 1.2rem;
-  }
   font-size: 1.4rem;
+  background: rgba(0, 0, 0, 0.035);
+  background-image: linear-gradient(
+      rgba(245, 245, 245, 1),
+      rgba(255, 255, 255, 0.88)
+    ),
+    url(${HexagonalBg});
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding-bottom: 4.2rem;
+  h3 {
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: ${colors.primary};
+    padding-bottom: 2.4rem;
+    margin: 4.2rem auto;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+    margin-bottom: 2.4rem;
+    font-family: "Anybody", sans-serif;
+    width: 70%;
+  }
+
+  ol {
+    margin-inline-start: 1.2rem;
+    margin-top: 2.4rem;
+
+    li {
+      padding-top: 1.2rem;
+    }
+
+    div {
+      font-weight: 100;
+      margin-top: 0.5rem;
+    }
+  }
 `;
 
 export const CoreTenetsWrapperInner = styled.div`
   display: flex;
-  gap: 1.2rem;
+  gap: 2.4rem;
+  padding-top: 1.2rem;
 `;
 
-export const WhatWeDoWrapper = styled.div``;
-export const WhatWeWantYouToDoWrapper = styled.div``;
+export const ListSectionTitleWrapper = styled.div<ListSectionTitleWrapperProps>`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+
+  h4 {
+    text-align: center;
+    width: fit-content;
+    position: relative;
+    z-index: 2;
+    font-size: 1.8rem;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    width: ${(props) => props.width || "114px"};
+    height: 20px;
+    background-color: #ccc;
+    border-radius: 2px;
+    z-index: 1;
+    bottom: 0px;
+    right: ${(props) => props.offset || "172px"};
+  }
+`;
+export const TenetListSection = styled.div``;
+
+export const TenetLisItemTitle = styled.p`
+  font-weight: bold;
+`;
--- frontend/src/pages/About/AboutPage.tsx
+++ frontend/src/pages/About/AboutPage.tsx
@@ -1,120 +1,166 @@
 import {
+  AboutIntroWrapperOuter,
   AboutIntroWrapper,
   AboutImageWrapper,
   MissionStatementWrapper,
   CoreTenetsWrapper,
   CoreTenetsWrapperInner,
-  WhatWeDoWrapper,
-  WhatWeWantYouToDoWrapper,
+  TenetListSection,
+  TenetLisItemTitle,
+  ListSectionTitleWrapper,
+  LinkWrapper,
 } from "./AboutPage.styled";
 import Title from "../../components/Title/Title";
 import { PageWrapper } from "../../PageWrapper.styled";
 import AboutImage from "../../assets/images/Amico-images/About-us-page-amico.svg";
+import { Link } from "react-router-dom";
+import ElipseImg from "../../assets/images/Ellipse-bg.svg";
+import BgSquaresAndTriangle from "../../assets/images/SquaresAndTriangles.svg";
 
 const AboutPage: React.FC = () => {
   return (
-    <PageWrapper>
+    <>
       <Title textContent="About" />
-      <AboutIntroWrapper>
-        <AboutImageWrapper>
-          <img src={AboutImage} alt="" />
-        </AboutImageWrapper>
+      <AboutIntroWrapperOuter>
+        <AboutIntroWrapper>
+          <AboutImageWrapper>
+            <img src={AboutImage} alt="" />
+          </AboutImageWrapper>
 
-        <MissionStatementWrapper>
-          <h2>Our Mission Statement</h2>
-          <p>
-            Empowering and educating IT specialists with unique and
-            unconventional tactics by leveraging insider knowledge to fast-track
-            success while fostering the mastery of core fundamentals through
-            hands-on development.
-          </p>
-        </MissionStatementWrapper>
-      </AboutIntroWrapper>
+          <MissionStatementWrapper>
+            <h2>Our Mission Statement</h2>
+            <p>
+              Empowering and educating IT specialists with unique and
+              unconventional tactics by leveraging insider knowledge to
+              fast-track success while fostering the mastery of core
+              fundamentals through hands-on development.
+            </p>
+            <LinkWrapper>
+              <Link to="#">Join Us</Link>
+            </LinkWrapper>
+          </MissionStatementWrapper>
+        </AboutIntroWrapper>
+        <img src={ElipseImg} className="elipse" alt="" />
+        <img src={BgSquaresAndTriangle} className="squares-triangle" alt="" />
+      </AboutIntroWrapperOuter>
       <CoreTenetsWrapper>
-        <p>
-          At Full Stack Fanatic, we strive to live up to a set of core tenets,
-          <br></br>
-          and hopefully help you do the same:
-        </p>
-        <CoreTenetsWrapperInner>
-          <WhatWeDoWrapper>
-            <h3>What We Do</h3>
-            <ul>
+        <PageWrapper>
+          <h3>
+            At Full Stack Fanatic, we strive to live up to a set of core tenets,
+            <br></br>
+            and hopefully help you do the same:
+          </h3>
+          <CoreTenetsWrapperInner>
+            <TenetListSection>
+              <ListSectionTitleWrapper offset="220px">
+                <h4>What We're About</h4>
+              </ListSectionTitleWrapper>
               <ol>
-                <b>1.</b> <u>Absolute Transparency </u>
-                <br></br>
-                We are committed to exposing industry dark patterns and
-                advocating for ethical business practices. Our goal is not just
-                to avoid sleazy practices ourselves but to educate our users on
-                how to recognize and overcome them.
+                <li>
+                  <TenetLisItemTitle>Absolute Transparency</TenetLisItemTitle>
+                  <div>
+                    We are committed to exposing industry dark patterns and
+                    advocating for ethical business practices. Our goal is not
+                    just to avoid sleazy practices ourselves but to educate our
+                    users on how to recognize and overcome them.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>
+                    Affordability and Flexibility
+                  </TenetLisItemTitle>
+
+                  <div>
+                    Quality education should be accessible to everyone. That’s
+                    why we offer affordable subscriptions, diverse access
+                    options for premium content, and valuable free resources to
+                    ensure everyone can grow their skills at their own pace.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>
+                    Fostering Collaboration and Networking
+                  </TenetLisItemTitle>
+
+                  <div>
+                    Learning is better together. Fullstack Fanatic provides a
+                    community where users can connect, collaborate, and support
+                    one another, creating a network of driven professionals who
+                    uplift each other.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>
+                    Revealing Insider Hacks and Game-Changing Tips
+                  </TenetLisItemTitle>
+
+                  <div>
+                    We equip you with resourceful, under-the-radar strategies to
+                    "game the system" and maximize efficiency—whether it’s
+                    finding powerful free tools, uncovering design shortcuts, or
+                    adopting techniques that give you a competitive edge.
+                  </div>
+                </li>
               </ol>
+            </TenetListSection>
+            <TenetListSection>
+              <ListSectionTitleWrapper width="150px">
+                <h4>What You Can Do to Succeed</h4>
+              </ListSectionTitleWrapper>
               <ol>
-                <b>2.</b> <u>Affordability and Flexibility</u>
-                <br></br>
-                Quality education should be accessible to everyone. That’s why
-                we offer affordable subscriptions, diverse access options for
-                premium content, and valuable free resources to ensure everyone
-                can grow their skills at their own pace.
+                <li>
+                  <TenetLisItemTitle>
+                    Take Pride in Quality Work
+                  </TenetLisItemTitle>
+
+                  <div>
+                    Strive for excellence in everything you create, delivering
+                    your work with care, research, and attention to detail.
+                    Becoming an expert means pushing yourself to go above and
+                    beyond to make an impact.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>
+                    Cultivate Emotional Regulation and Resilience
+                  </TenetLisItemTitle>
+
+                  <div>
+                    Programming and IT require patience and perseverance.
+                    Embrace the challenges, develop strategies to manage
+                    frustration, and grow through every obstacle you encounter.
+                    This journey is as much about personal growth as it is about
+                    technical skill.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>Adopt a Growth Mindset</TenetLisItemTitle>
+
+                  <div>
+                    Continuously seek to improve, stay curious, and embrace
+                    lifelong learning. Whether you’re just starting out or a
+                    seasoned developer, there’s always more to learn and
+                    opportunities to refine your craft.
+                  </div>
+                </li>
+                <li>
+                  <TenetLisItemTitle>
+                    Think Ethically and Act Purposefully
+                  </TenetLisItemTitle>
+
+                  <div>
+                    Let your work reflect integrity and purpose. Whether you're
+                    designing websites, writing code, or securing systems, make
+                    decisions that align with values of honesty, accessibility,
+                    and inclusivity.
+                  </div>
+                </li>
               </ol>
-              <ol>
-                <b>3.</b> <u>Fostering Collaboration and Networking</u>
-                <br></br>
-                Learning is better together. Fullstack Fanatic provides a
-                community where users can connect, collaborate, and support one
-                another, creating a network of driven professionals who uplift
-                each other.
-              </ol>
-              <ol>
-                <b>4.</b> <u>Revealing Insider Hacks and Game-Changing Tips</u>
-                <br />
-                We equip you with resourceful, under-the-radar strategies to
-                "game the system" and maximize efficiency—whether it’s finding
-                powerful free tools, uncovering design shortcuts, or adopting
-                techniques that give you a competitive edge.
-              </ol>
-            </ul>
-          </WhatWeDoWrapper>
-          <WhatWeWantYouToDoWrapper>
-            <h3>What We Want You to Do</h3>
-            <ul>
-              <ol>
-                <b>1.</b> <u>Take Pride in Quality Work </u>
-                <br></br>
-                Strive for excellence in everything you create, delivering your
-                work with care, research, and attention to detail. Becoming an
-                expert means pushing yourself to go above and beyond to make an
-                impact.
-              </ol>
-              <ol>
-                <b>2.</b> <u>Cultivate Emotional Regulation and Resilience</u>
-                <br></br>
-                Quality education should be accessible to everyone. That’s why
-                Programming and IT require patience and perseverance. Embrace
-                the challenges, develop strategies to manage frustration, and
-                grow through every obstacle you encounter. This journey is as
-                much about personal growth as it is about technical skill.
-              </ol>
-              <ol>
-                <b>3.</b> <u>Adopt a Growth Mindset</u>
-                <br></br>
-                Continuously seek to improve, stay curious, and embrace lifelong
-                learning. Whether you’re just starting out or a seasoned
-                developer, there’s always more to learn and opportunities to
-                refine your craft.
-              </ol>
-              <ol>
-                <b>4.</b> <u>Think Ethically and Act Purposefully</u>
-                <br />
-                Let your work reflect integrity and purpose. Whether you're
-                designing websites, writing code, or securing systems, make
-                decisions that align with values of honesty, accessibility, and
-                inclusivity.{" "}
-              </ol>
-            </ul>
-          </WhatWeWantYouToDoWrapper>
-        </CoreTenetsWrapperInner>
+            </TenetListSection>
+          </CoreTenetsWrapperInner>
+        </PageWrapper>
       </CoreTenetsWrapper>
-    </PageWrapper>
+    </>
   );
 };
 
