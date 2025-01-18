219b2f895c8471ac55eca6c78d308cd2902d9957
Ildi Dvorani <dvoranii@ildidev.com>
2025-01-17 21:59:33 -0500

    changed single db connection to pool connection to avoid timeout issues

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,23 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const createConnection = async () => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user:
-      process.env.NODE_ENV === "production"
-        ? "dbuser"
-        : process.env.NODE_ENV === "staging"
-        ? "staging_user"
-        : "root",
-    password: process.env.DB_PW,
-    database: "tutorials_db",
-  });
+const pool = mysql.createPool({
+  host: "localhost",
+  user:
+    process.env.NODE_ENV === "production"
+      ? "dbuser"
+      : process.env.NODE_ENV === "staging"
+      ? "staging_user"
+      : "root",
+  password: process.env.DB_PW,
+  database: "tutorials_db",
 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+pool.on("connection", () => {
   console.log("MySQL Connected...");
-  return connection;
-};
+});
 
-const connection = createConnection();
-
-export default connection;
+export default pool;
